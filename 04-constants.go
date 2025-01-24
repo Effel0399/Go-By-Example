1efed9b1a767b935db784f829c6db1564bb76414
FNH99 <148479741+FNH99@users.noreply.github.com>
2025-01-24 22:20:07 +0700

    added comments to 04

--- 04-constants.go
+++ 04-constants.go
@@ -16,6 +16,10 @@ func main04() {
 	fmt.Println(d)
 
 	fmt.Println(int64(d))
+	// numeric constant has no type until given one
+	// such as by an explicit conversion
 
 	fmt.Println(math.Sin(n))
+	// a number can be given a type in context that requires it
+	// example, math.Sin expects a float64
 }
