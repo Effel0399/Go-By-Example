c19a812d4e8b30ed53cf4b2e082f9c9ef5ad210e
FNH99 <148479741+FNH99@users.noreply.github.com>
2025-01-24 21:54:48 +0700

    02-values

---
+++ 02-values.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main02() {
+	fmt.Println("go" + "lang")
+
+	fmt.Println("1+1 =", 1+1)
+	fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+	fmt.Println(true && false) // AND
+	fmt.Println(true || false) // OR
+	fmt.Println(!true)         // NOT
+}
